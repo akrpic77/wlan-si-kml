83c4c1123e65eeb6c6f02401f53447d6fa446462
Andrej Krpic <andrej.krpic@gmail.com>
2014-04-21 22:08:51 +0200

    Only replace kml file if new one is successfuly built.

--- kmlgen.sh
+++ kmlgen.sh
@@ -3,7 +3,7 @@
 while :
 do
     ./nodes.py > generated-nodes.kml-
-    mv generated-nodes.kml- www/generated-nodes.kml
+    [ $? == 0 ] && mv generated-nodes.kml- www/generated-nodes.kml
     sleep 60
 done
 
