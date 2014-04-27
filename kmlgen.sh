27ae97f2567921ec82a8fe79645039303f0a224a
Andrej Krpic <akrpic77@gmail.com>
2014-04-27 21:30:49 +0200

    use -eq for integer comparison.

--- kmlgen.sh
+++ kmlgen.sh
@@ -3,7 +3,7 @@
 while :
 do
     ./nodes.py > generated-nodes.kml-
-    [ $? == 0 ] && mv generated-nodes.kml- www/generated-nodes.kml
+    [ $? -eq 0 ] && mv generated-nodes.kml- www/generated-nodes.kml
     sleep 60
 done
 
